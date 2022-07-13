daf73be7e5c022ce389cc230c7f78211445a36cd
Muhammadyusuf <ibnhasaniy@gmail.com>
2022-07-13 22:00:42 +0500

    feat: solve 7-kyu factorial task

---
+++ 7-kyu-factorial.js
@@ -0,0 +1,12 @@
+// 7 kyu factorial
+// https://www.codewars.com/kata/54ff0d1f355cfd20e60001fc
+
+const factorial = (n) => {
+  if(n < 0 || n > 12) throw new RangeError('You must input from 0 to 12');
+  
+  let result = 1;
+  for(let i = 1; i < n + 1; i++){
+    result *= i;
+  }
+  return result;
+}
