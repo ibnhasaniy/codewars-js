fced0814aecbacad0322c098f0bced180a4e6778
Muhammadyusuf <ibnhasaniy@gmail.com>
2022-07-14 21:37:09 +0500

    feat: 8-kyu return negative

---
+++ 8-kyu-return-negative.js
@@ -0,0 +1,9 @@
+// 8 kyu return negative
+// https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+function makeNegative(num) {
+  return num < 0 ? num : -num
+
+//   if(num < 0) return num;
+//   return -num
+}
