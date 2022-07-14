f2213acac2e94479d72c5b4db049b3090fef93d8
Muhammadyusuf <ibnhasaniy@gmail.com>
2022-07-14 23:39:23 +0500

    feat: 8 kyu Convert a String to a Number!

---
+++ 8-kyu-convert-a-string-to-a-number.js
@@ -0,0 +1,7 @@
+// 8 kyu Convert a String to a Number!
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/
+
+const stringToNumber = (str) => +str; // :)
+
+// You can copy built-in Number constructor to stringToNumber
+// stringToNumber = Number
