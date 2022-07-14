f42dacc5cba7807ca2d0ba5fed4cb0241ce2b0c7
Muhammadyusuf <ibnhasaniy@gmail.com>
2022-07-14 23:26:06 +0500

    feat: 8-kyu-sum-arrays.js

---
+++ 8-kyu-sum-arrays.js
@@ -0,0 +1,15 @@
+// 8-kyu Sum Arrays
+// https://www.codewars.com/kata/53dc54212259ed3d4f00071c
+
+const sum = (arr) => arr.reduce((a, b) => a + b, 0)
+
+
+// const sum = (numbers) => {
+//   if (!numbers || !numbers.length) return 0;
+
+//   let sum = 0;
+//   for (const idx of numbers) {
+//     sum += idx;
+//   }
+//   return sum;
+// };
