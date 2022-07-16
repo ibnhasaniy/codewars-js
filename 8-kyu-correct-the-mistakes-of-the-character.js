045690aa65fff38214960cd9608f9065d9972bc5
Muhammadyusuf <ibnhasaniy@gmail.com>
2022-07-16 21:33:15 +0500

    feat: 8 kyu Correct the mistakes of the character recognition software

---
+++ 8-kyu-correct-the-mistakes-of-the-character.js
@@ -0,0 +1,4 @@
+// 8 kyu Correct the mistakes of the character recognition software
+// https://www.codewars.com/kata/577bd026df78c19bca0002c0/
+
+const correct = (str) => str.replace(/5/g,"S").replace(/0/g,"O").replace(/1/g,"I")
