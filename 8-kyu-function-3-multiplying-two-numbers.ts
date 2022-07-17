eba59bb9c2f40f855d67423987e48ce7898342cc
Muhammadyusuf <ibnhasaniy@gmail.com>
2022-07-17 23:26:32 +0500

    feat: 8-kyu Function 3 - multiplying two numbers

---
+++ 8-kyu-function-3-multiplying-two-numbers.ts
@@ -0,0 +1,6 @@
+// 8 kyu Function 3 - multiplying two numbers
+// https://www.codewars.com/kata/523b66342d0c301ae400003b/
+
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
