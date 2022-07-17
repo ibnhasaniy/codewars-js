aec8e8c00fea1bfc7b811196354efff108ec3d75
Muhammadyusuf <ibnhasaniy@gmail.com>
2022-07-17 23:10:41 +0500

    feat: 8-kyu FIXME: Replace all dots.ts

---
+++ 8-kyu-fixme-replace-all-dots.ts
@@ -0,0 +1,6 @@
+// 8 kyu FIXME: Replace all dots
+// https://www.codewars.com/kata/596c6eb85b0f515834000049/
+
+export function replaceDots(str: string): string {
+  return str.replace(/\./g, '-');
+}
